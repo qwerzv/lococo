b602cd4cca4ec933267d6706d53de2e2658a087d
qwerzv <bcjyp000325@naver.com>
2023-11-14 15:41:37 +0900

    test

---
+++ src/component/FooterComponent.js
@@ -0,0 +1,11 @@
+import "../style/FooterComponent.css";
+
+const FooterComponent = () => {
+  return (
+    <div className="FooterComponent">
+      <h1>Footer 입니다.</h1>
+    </div>
+  );
+};
+
+export default FooterComponent;
